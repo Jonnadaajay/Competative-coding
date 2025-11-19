d8b0c6d25abaf70c9b585ff4a14feff4d671ea7c
Jonnada Ajay <jonnadaajay9390@gmail.com>
2025-11-19 14:40:20 +0530

    Implement function to replace 0's with 5

---
+++ Replace all 0's with 5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convertFive(self, n):
+        # Code here
+        str_n = str(n)
+        res = ''
+        
+        for char in str_n:
+        
+            if char == '0':
+                res = res + '5'
+        
+            else:
+                res += char
+        
+        return res
